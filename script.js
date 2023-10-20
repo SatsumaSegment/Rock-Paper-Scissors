8566aa3503a1d13423aabab3f41e0ac3a583e5e4
partialarts <145933508+jemlawX@users.noreply.github.com>
2023-10-20 19:51:36 +0100

    fix bug

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 console.log (computerChoice);
 
-var rPS = ["R","P","S"];
+var rPS = ["r","p","s"];
 var playerWins = 0;
 var computerWins = 0;
 var tie = 0;
 
 for (var i = 0; i < 10; i++) {
-        var choice = prompt("Enter R, P or S");
+        var choice = prompt("Enter r, p or s");
         var computerChoice = rPS [Math.floor(Math.random()*rPS.length)];
     if (choice == computerChoice) {
         tie++;
-        alert("Draw") 
-    } else if (choice == "R" && computerChoice == "P") {
+        alert("Draw + " + "Computer also chose " + computerChoice)
+    } else if (choice == "r" && computerChoice == "p") {
         computerWins++;
-        alert("Computer wins " + "Computer chose " + computerChoice)
-    } else if (choice == "R" && computerChoice == "S") {
+        alert("Computer wins - " + "Computer chose " + computerChoice)
+    } else if (choice == "r" && computerChoice == "s") {
         playerWins++;
-        alert("You win" + "Computer chose " + computerChoice)
-    } else if (choice == "S" && computerChoice == "P") {
+        alert("You win - " + "Computer chose " + computerChoice)
+    } else if (choice == "s" && computerChoice == "p") {
         playerWins++;
-        alert("You win" + "Computer chose " + computerChoice)
-    } else if (choice == "S" && computerChoice == "R") {
+        alert("You win - " + "Computer chose " + computerChoice)
+    } else if (choice == "s" && computerChoice == "r") {
         computerWins++;
-        alert("Computer wins " + "Computer chose " + computerChoice)
-    } else if (choice == "P" && computerChoice == "R") {
+        alert("Computer wins - " + "Computer chose " + computerChoice)
+    } else if (choice == "p" && computerChoice == "r") {
         playerWins++;
-        alert("You win" + "Computer chose " + computerChoice)
-    } else if (choice == "P" && computerChoice == "S") {
+        alert("You win - " + "Computer chose " + computerChoice)
+    } else if (choice == "p" && computerChoice == "s") {
         computerWins++;
-        alert("Computer wins" + "Computer chose " + computerChoice) 
+        alert("Computer wins - " + "Computer chose " + computerChoice) 
     } else {
         alert("Go back and do it properly")
     }
